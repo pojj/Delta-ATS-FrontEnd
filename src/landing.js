071a3e47d758af9dd818ca7c3e8fc9673f8d9826
William Song <williambsong@gmail.com>
2022-05-11 18:33:19 -0700

    FIRST COMMIT

--- src/landing.js
+++ src/landing.js
@@ -4,53 +4,60 @@ import { Image } from 'react-bootstrap';
 
 
 
-export class Landing extends React.Component{
-    render(){
+export class Landing extends React.Component {
+    render() {
         //this is the stuff at the top of the entire landing page
         //just sort of made sense to make it an object of some sort so its easier to move around
-        let topPart = 
-        (
-            <div className = "topPart">
-                <div className = "logo">
-                    <Image src = "./DeltaLogo.png" className = "logoImg"/>
+        let topPart =
+            (
+                <div className="topPart">
+                    <div className="logo">
+                        <Image src="./DeltaLogo.png" className="logoImg" />
+                    </div>
+                    <div className="navButtons">
+
+                        <a href='https://www.google.com/' className="topButton textButton">
+                            Register
+                        </a>
+
+                        <a href='https://www.google.com/' className="topButton textButton">
+                            Login
+                        </a>
+
+                        <a href='https://www.google.com/' className="topButton textButton">
+                            About
+                        </a>
+
+                        <a href='https://www.google.com/' className="topButton textButton">
+                            Contact Us
+                        </a>
+                    </div>
                 </div>
-                <div className = "navButtons">
+            );
 
-                    <button className = "topButton register rounded-pill">
-                        Register
-                    </button>
-
-                    <a href='https://www.google.com/' className = "topButton textButton">
-                        Login
-                    </a>
-
-                    <a href='https://www.google.com/' className = "topButton textButton">
-                        About
-                    </a>
-
-                    <a href='https://www.google.com/' className = "topButton textButton">
-                        Contact Us
-                    </a>
-                </div>
-            </div>
-        );         
-
-        return(
+        return (
             <div>
                 {topPart}
-                
-                <div className = "content">
+
+                <div className="content">
                     <div className='titleBox'>
-                        <h1 className = "title">Catchphrase that's as long as this</h1>
+                        <h1 className="title">Catchphrase that's as long as this</h1>
                     </div>
-                    <div className = "divider"/> <div className = "divider"/> <div className = "divider"/>
-                    <div className = "textBox">
-                        <p className = "bottomText">
-                            as an example: among the industries most <br/>
-                            respected, Delta Controls offeres you the <br/>
+                    <div className="divider" /> <div className="divider" /> <div className="divider" />
+
+                    <div className="textBox">
+                        <p className="bottomText">
+                            as an example: among the industries most <br />
+                            respected, Delta Controls offeres you the <br />
                             coolest career path possible
                         </p>
                     </div>
+
+
+
+                    <button className="register rounded-pill">
+                        Apply Now
+                    </button>
                 </div>
             </div>
         );
