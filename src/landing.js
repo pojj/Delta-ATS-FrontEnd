f76a3e33942f1fea0e7f0cd471f437bb1387a1d9
trisexual <mrthomaszheng@gmail.com>
2022-05-11 18:33:21 -0700

    test is this on a new branch

--- src/landing.js
+++ src/landing.js
@@ -1,1 +1,14 @@
-//testing
+import React from 'react';
+import "./landing.css";
+
+
+
+export class Landing extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1 className = "big">among us</h1>
+            </div>
+        );
+    }
+}
