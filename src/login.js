43367184a791240e8bfa315fabece327aed1b3c1
William Song <williambsong@gmail.com>
2022-05-13 16:45:36 -0700

    prototype (textboxes do nothing)

--- src/login.js
+++ src/login.js
@@ -1,2 +1,29 @@
-// test new branch
-// pojjj
+import React from 'react';
+import './Login.css';
+
+class Login extends React.Component {
+  render(){
+    return (
+      <form>
+      <h2>Sign In</h2>
+      <div>
+        <label>Email:</label><br/>
+        <input className='text-box'
+          placeholder="Enter email"
+        />
+      </div>
+      <div>
+        <label>Password:</label><br/>
+        <input className='text-box'
+          placeholder="Enter password"
+        />
+      </div>
+      <button>
+        Submit
+      </button>
+    </form>
+    );
+  }
+}
+
+export default Login;
