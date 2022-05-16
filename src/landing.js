fa965e0ffda53bfd4ed98f873bff8ccd9f8cc9a7
trisexual <mrthomaszheng@gmail.com>
2022-05-15 21:42:13 -0700

    i accidentaly commited to the wrong branch

--- src/landing.js
+++ src/landing.js
@@ -1,15 +1,45 @@
 import React from 'react';
 import "./landing.css";
+import { Image } from 'react-bootstrap';
 
 
 
 export class Landing extends React.Component{
     render(){
+        //this is the stuff at the top of the entire landing page
+        //just sort of made sense to make it an object of some sort so its easier to move around
+        //how
+        let topPart = 
+        (
+            <div className = "topPart">
+                <div className = "logo">
+                    <Image src = "./DeltaControlsLogo.png" className = "logoImg"/>
+                </div>
+                <div className = "navButtons">
+
+                    <button className = "topButton register rounded-pill">
+                        Register
+                    </button>
+
+                    <a href='https://www.google.com/' className = "topButton textButton">
+                        Login
+                    </a>
+
+                    <a href='https://www.google.com/' className = "topButton textButton">
+                        About
+                    </a>
+
+                    <a href='https://www.google.com/' className = "topButton textButton">
+                        Contact Us
+                    </a>
+                </div>
+            </div>
+        );         
+
         return(
             <div>
-                <div className = "topPart">
-                    
-                </div>
+                {topPart}
+                
                 <div className = "content">
                     <div className='titleBox'>
                         <h1 className = "title">Catchphrase that's as long as this</h1>
