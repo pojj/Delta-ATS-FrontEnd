e118272b60e21709d68b253776114c2cae8d4ef9
Sylvonir <thomasmjzeng@gmail.com>
2022-05-19 13:57:54 -0700

    File naming form alignment logo colour corrections

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -31,27 +31,32 @@ class LoginForm extends React.Component {
             <div>
                 <Header btn="Click Me" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" />
 
-                <form onSubmit={this.handleSubmit}>
+                <form onSubmit={this.handleSubmit} className="form-area center">
                     <h1><b>Login to your account</b></h1>
                     <input
                         placeholder="Email"
+                        type='text'
+                        size='100px'
                         onChange={this.handleEmailChange}
                         className="textbox" />
                     <br />
 
                     <input
                         placeholder="Password"
+                        type="password"
                         onChange={this.handlePasswordChange}
                         className="textbox" />
                     <br />
 
-                    <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Forgot My Password</a>
-                    <br />
+                    <div className="right-align">
+                        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Forgot My Password</a>
+                        <br />
 
-                    {/* This is using the pre-made button Components from Bootstrap */}
-                    <Button type="submit" variant="danger" onClick={this.handleSubmit} className="rounded-pill">
-                        Login
-                    </Button>
+                        {/* This is using the pre-made button Components from Bootstrap */}
+                        <Button type="submit" variant="danger" onClick={this.handleSubmit} className="rounded-pill login-button">
+                            Login
+                        </Button>
+                    </div>
                 </form>
             </div>
         );
